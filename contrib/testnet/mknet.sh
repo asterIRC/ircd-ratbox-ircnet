78d6996699ec0958ac53b3cd6d6c17b496b9293a
karel.tuma <karel.tuma@55836c16-fd44-11de-9d47-03d50c098576>
2010-01-19 02:08:13 +0000

    Add 4 more servers to the topogy, currently 10 servers of 3 masks in chain with various 2.11/ratbox combinations inbetween.
    git-svn-id: http://ircd-ratbox-ircnet.googlecode.com/svn/trunk@98 55836c16-fd44-11de-9d47-03d50c098576

--- contrib/testnet/mknet.sh
+++ contrib/testnet/mknet.sh
@@ -7,14 +7,15 @@ chmod +x *.sh
 # suffixes of masked clusters
 sufa="${OPERNAME}a"
 sufb="${OPERNAME}b"
+sufc="${OPERNAME}c"
 
 # created topology is like
 #	hub1-211.sda [6667]------.------hub1-ratbox.sdb [6607]
 #	|			/ \	|
-#	hub2-ratbox.sda [6668]-'   `----hub2-211.sdb [6608]
-#	|				|
-#	leaf-211.sda [6669]		leaf-ratbox.sdb [6609]
-
+#	hub2-ratbox.sda [6668]-'   `----hub2-211.sdb [6608]-------hub1-211.sdc[6617]---hub2-211.sdc[6618]----leaf1-ratbox.sdc[6619]
+#	|		       		|                              \__leaf2-ratbox.sdc[6620]
+#	leaf-211.sda [6669]	 	leaf-ratbox.sdb [6609]       
+#				                                     
 #### left part
 ./mk211.sh "hub1-211.$sufa" 6667 "
 *.$sufb:6607:1
@@ -49,10 +50,33 @@ leaf-ratbox.$sufb:
 *.$sufa:6668:1
 hub1-ratbox.$sufb:6607
 leaf-ratbox.$sufb:
+*.$sufc:6617:1
 "
 
 ./mkrb.sh "leaf-ratbox.$sufb" 6609 "
 hub1-ratbox.$sufb:6607
 hub2-211.$sufb:6608
+"
+
+#### sdc part
+./mk211.sh "hub1-211.$sufc" 6617 "
+*.$sufb:6608:1
+hub2-211.$sufc:6618
+leaf1-ratbox.$sufc:
+leaf2-ratbox.$sufc:
+"
+
+./mk211.sh "hub2-211.$sufc" 6618 "
+hub1-211.$sufc:6617
+leaf1-ratbox.$sufc:
+leaf2-ratbox.$sufc:
+"
+
+./mkrb.sh "leaf1-ratbox.$sufc" 6619 "
+hub2-211.$sufc:6618
+"
+
+./mkrb.sh "leaf2-ratbox.$sufc" 6620 "
+hub1-211.$sufc:6617
 "
 
